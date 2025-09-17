67efa9676d80cd3bf38f3ec8565b0e496ceb6dd0
ya_yarek <kvachonoky@gmail.com>
2025-09-17 23:37:56 +0300

    second commit

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -11,7 +11,7 @@ let dice1 = Math.ceil(Math.random() * 6); // your code
 let dice2 = Math.ceil(Math.random() * 6); // your code
 let isWinningDouble // your code
 
-if (dice1 === dice2 && ((dice1 + dice2) > 3)) {
+if (dice1 === dice2 && dice1 > 3 && dice2 > 3) {
     isWinningDouble = true;
 } else {
     isWinningDouble = false;
