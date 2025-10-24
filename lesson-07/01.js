64061c3b241488695b98faddbfcae2d994c3693a
ya_yarek <kvachonoky@gmail.com>
2025-10-24 23:26:24 +0300

    second commit

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -13,7 +13,28 @@
 */
 
 const WEB_TECH_IMAGES = [
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const next = document.getElementById("next-button")
+const prev = document.getElementById("prev-button")
+const image = document.getElementById("web-tech-image")
+let index = 0;
+next.addEventListener('click', function () {
+    index++
+    if (index === WEB_TECH_IMAGES.length) {
+        index = 0
+    }
+    image.src = WEB_TECH_IMAGES[index]
+
+})
+prev.addEventListener('click', function () {
+    index--
+    if (index === -1) {
+        index = WEB_TECH_IMAGES.length - 1
+    }
+    image.src = WEB_TECH_IMAGES[index]
+
+})
